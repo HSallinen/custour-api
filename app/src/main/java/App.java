194d7d3cf23c85405d7fa8dc532603e17e675cf5
mntdent <mntadent@gmail.com>
2024-12-04 10:36:49 +0200

    Cleaned even more

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -5,7 +5,7 @@ package org.custour;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Heeeee World!";
     }
 
     public static void main(String[] args) {
